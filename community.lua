a5862f8d68641f6a70f1f981d93e3211656cf3a2
Lorenzo Argentieri <uni.lorenzo.a@gmail.com>
2025-01-26 12:26:45 +0100

    Create community.lua

---
+++ community.lua
@@ -0,0 +1,19 @@
+--if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  -- import/override with your plugins folder
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.utility.hover-nvim" },
+  { import = "astrocommunity.scrolling.mini-animate" },
+  { import = "astrocommunity.split-and-window.neominimap-nvim" },
+  { import = "astrocommunity.utility.noice-nvim" },
+  --{ import = "astrocommunity.bars-and-lines.lualine-nvim" },
+  --{ import = "astrocommunity.pack.nvchad-ui" },
+}
