324eac0c623c02e4d4f0eaea9c2669576e3b7c1a
Lorenzo Argentieri <uni.lorenzo.a@gmail.com>
2025-02-13 17:29:39 +0100

    Update community.lua

--- community.lua
+++ community.lua
@@ -24,4 +24,5 @@ return {
   { import = "astrocommunity.editing-support.refactoring-nvim" },
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
   { import = "astrocommunity.media.drop-nvim" },
+  { import = "astrocommunity.media.codesnap-nvim" },
 }
